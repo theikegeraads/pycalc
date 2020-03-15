efaf115ddec7d86ae33718af4d71bb0d48202296
Theike Geraads <58556805+theikegeraads@users.noreply.github.com>
2020-03-15 22:28:11 +0100

    "Finished" calculator.py

--- calculator.py
+++ calculator.py
@@ -1,31 +1,44 @@
 import math
-n1 = int(input("Getal 1: "))
-module = input("+, -, *, /, sq: ")
-n2 = int(input("Getal 2: "))
+n1 = int(input('Getal 1: '))
+operator = input('+, -, *, /, sq, pow: ')
+
+wort = math.sqrt(n1)
+if operator == 'sq':
+    print(wort)
+    exit()
+else:
+    pass
+
+kwad = pow(n1, 2)
+if operator == 'pow':
+    print(kwad)
+    exit()
+else:
+    pass
+
+n2 = int(input('Getal 2: '))
+
 plus = n1 + n2
 min = n1 - n2
 keer = n1 * n2
 deel = n1 / n2
-kwad = math.pow(n1, n2)
 
-if module == "+":
+if operator == '+':
     print(plus)
 else:
     pass
 
-if module == "-":
+if operator == '-':
     print(min)
 else:
     pass
-    
-if module == "*":
+
+if operator == '*':
     print(keer)
 else:
     pass
 
-if module == "/":
+if operator == '/':
     print(deel)
 else:
     pass
-if module == "sq":
-    print(kwad)
